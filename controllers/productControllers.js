bfc833021a7b8587090857efa06dedaeeb251ec7
Avijit Karmaker <ajkacca@gmail.com>
2023-06-22 12:30:47 +0300

    add single route for product

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -5,3 +5,10 @@ export const getProducts=AsyncHandler((req,res,next)=>{
         message: "will get all the products"
     })
 }) 
+
+
+export const getProduct= AsyncHandler((req,res,next)=>{
+    res.status(200).json({
+        message:"will get single product"
+    })
+})
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { getProducts } from "../controllers/productControllers.js";
+import { getProducts, getProduct } from "../controllers/productControllers.js";
 
 const router= express.Router();
 
 router.route("/").get(getProducts);
-
+router.route("/:id").get(getProduct);
 export default router;
