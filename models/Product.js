8e17269d8adcbe3f1182afc68bc7fd6a1df19aa8
Avijit Karmaker <ajkacca@gmail.com>
2023-06-22 14:19:31 +0300

    update model

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,16 @@
 import mongoose from "mongoose";
 
 const ProductSchema= mongoose.Schema({
-
+    name:{
+        type:String,
+        required:[true,"product name is requires"],
+        maxLength: [50, "Product name cannot exceed 50 characters"]
+    },
+    price:{
+        type:Number,
+        require:[true,"product price is required"],
+        min:[1,"product price cant be less than 0"]
+    }
 
 
 })
