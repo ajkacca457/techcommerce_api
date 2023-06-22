f9d3d4397b2c372a6cc92cdb012be911713a4798
Avijit Karmaker <ajkacca@gmail.com>
2023-06-22 12:18:09 +0300

    add async handler

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,6 +1,7 @@
-export const getProducts= (req,res,next)=>{
+import AsyncHandler from "../middlewares/AsyncHandler.js"
+
+export const getProducts=AsyncHandler((req,res,next)=>{
     res.status(200).json({
         message: "will get all the products"
     })
-
-}
+}) 
