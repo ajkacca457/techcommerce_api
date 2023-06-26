75018a08f3bb8d396ee18600c1ff11f9ae54c7e5
Avijit Karmaker <ajkacca@gmail.com>
2023-06-26 10:51:39 +0300

    update product schema with category and shipping

--- models/Product.js
+++ models/Product.js
@@ -3,12 +3,17 @@ import mongoose from "mongoose";
 const ProductSchema= mongoose.Schema({
     name:{
         type:String,
-        required:[true,"product name is requires"],
+        required:[true,"product name is required"],
         maxLength: [50, "Product name cannot exceed 50 characters"]
+    },
+    description:{
+        type:String,
+        required:[true,"product description is required"],
+        maxLength: [200, "Product name cannot exceed 200 characters"]
     },
     price:{
         type:Number,
-        require:[true,"product price is required"],
+        required:[true,"product price is required"],
         min:[1,"product price cant be less than 0"]
     },
     image:{
@@ -19,6 +24,15 @@ const ProductSchema= mongoose.Schema({
         type:Number,
         min:[1,"rating cant be less than 1"],
         max:[6,"rating cant be more than 5"],
+        required:[true,"product rating is required"],
+    },
+    shipping:{
+        type:Boolean,
+        default:false
+    },
+    category:{
+        type:String,
+        enum:["laptop","tablet","desktop","mobile"]
     }
 
 })
