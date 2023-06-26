7ce6291d3426e0088b5b1b54e04fbce876f39608
Avijit Karmaker <ajkacca@gmail.com>
2023-06-26 10:44:16 +0300

    adding model values

--- models/Product.js
+++ models/Product.js
@@ -10,8 +10,16 @@ const ProductSchema= mongoose.Schema({
         type:Number,
         require:[true,"product price is required"],
         min:[1,"product price cant be less than 0"]
+    },
+    image:{
+        type:String,
+        default:"placeholder.jpg"
+    },
+    rating:{
+        type:Number,
+        min:[1,"rating cant be less than 1"],
+        max:[6,"rating cant be more than 5"],
     }
-
 
 })
 
