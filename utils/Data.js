3541cb21efca38fd007689ab67705b6caa126cd2
Avijit Karmaker <ajkacca@gmail.com>
2023-06-28 13:33:33 +0300

    update product data

---
+++ utils/Data.js
@@ -0,0 +1,102 @@
+export const products= [
+{
+    name:"Legion 5 15ARH7H",
+    description:'Powerful Ryzen 7 6800H processor Full-power RTX 3070 8GB graphics card (TGP 140W) 16GB DDR5 RAM, 1TB PCIe SSD15.6" WQHD IPS 165Hz panel with G-Sync',
+    price :2000,
+    rating :3.5,
+    shipping:true,
+    category:'laptop',
+    company: 'Lenevo',
+    featured :false
+},
+{
+    name:"Asus F415 F415JA-EB2244W",
+    description: 'Intel Core i7-1065G7, 16 Gt, 512 Gt SSD14" non-reflective Full HD screen An efficient basic machine',
+    price :749,
+    rating :4,
+    shipping:false,
+    category:'laptop',
+    company: 'Asus',
+    featured :false
+},
+{
+    name:"Samsung Galaxy Book2 NP750XED-KC1SE",
+    description: 'Wide 15-inch screen Light and efficient Fast and efficient SSD disk and expansion slot',
+    price :799,
+    rating :5,
+    shipping:false,
+    category:'laptop',
+    company: 'Samsung',
+    featured :false
+},
+{
+    name:"Zenbook Pro 14 Duo OLED UX8402ZE-M3100X",
+    description: 'Intel Core i9-12900H, 32 Gt, 1 Tt SSD 14" 2.8K OLED 120 Hz touch screen NVIDIA GeForce RTX 3050 Ti',
+    price :3000,
+    rating :5,
+    shipping:true,
+    category:'laptop',
+    company: 'Asus',
+    featured :false
+},
+{
+    name:"MacBook Pro 13 M2 512Gb",
+    description: 'M2 - plenty of power, always on the go Active cooling ensures lightning-fast performance Working together, macOS and M2 bring more speed and responsiveness to your most important apps - including Microsoft 365 and your favorite iOS apps Deliveries start in week 26-27',
+    price :1899,
+    rating :5,
+    shipping:true,
+    category:'laptop',
+    company: 'Apple',
+    featured :true
+},
+{
+    name:"Tab M10 Plus 3rd gen LTE",
+    description: '10.61" 2K IPS screen 128GB storage and up to 1TB memory card support Stylish and modern aluminum frame Pure Android 12 and separate reading mode',
+    price :399,
+    rating :3.6,
+    shipping:false,
+    category:'tablet',
+    company: 'Lenevo',
+    featured :false
+},
+{
+    name:"Galaxy Tab S8 Ultra 5G 512Gt",
+    description: 'Vibrant display with a 120 Hz refresh rate, which has an extremely fast response time A very sharp front camera whose ultra-wide angle makes e.g. video meetings more realistic The S Pen included in the sales package is a versatile and intuitive tool for quick work and creation Works seamlessly with other Samsung Galaxy devices',
+    price :1699,
+    rating :4.2,
+    shipping:true,
+    category:'tablet',
+    company: 'Samsung',
+    featured :false
+},
+{
+    name:"iPad Mini 2021 256 Gt 5G",
+    description: 'Power for every task with the new A15 Bionic chip New full screen. Style from edge to edge. Ultra wide angle camera. More opportunities.',
+    price :909,
+    rating :4.1,
+    shipping:true,
+    category:'tablet',
+    company: 'Apple',
+    featured :false
+},
+{
+    name:"Galaxy Z Flip4 5G 128GB",
+    description: 'A completely unique design, loved all over the world When opened, you get to experience the full-size 120Hz 6.7-inch screen The 1.9-inch front screen also makes sure you don"t miss the most important things When folded, it becomes almost half the size of a standard smartphone When partially opened, Flex Mode offers many new and intelligent ways of use',
+    price :1149,
+    rating :4.8,
+    shipping:false,
+    category:'mobile',
+    company: 'Samsung',
+    featured :true
+},
+{
+    name:"iPhone 13 Pro Max 1 Tt 5G",
+    description: '6.7 inch Super Retina XDR display with ProMotion Hard-class durability with Ceramic Shield A15 Bionic chip for lightning-fast performance 5G for super-fast downloads and high-quality streams Professional camera system and TrueDepth front camera Best waterproof rating in the industry (IP68) Supports MagSafe accessories Great performance in Elisa"s 5G network',
+    price :1860,
+    rating :5,
+    shipping:true,
+    category:'mobile',
+    company: 'Apple',
+    featured :true
+},
+]
